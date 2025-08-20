5903233073fc28232d80822715cee74965f8c253
Simhateja17 <mg8751721@gmail.com>
2025-08-20 21:18:22 +0530

    Hi

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -87,10 +87,10 @@ export const Footer = () => {
                             <h4 className="heading-5 text-white mb-6">Quick Links</h4>
                             <nav className="space-y-3">
                                 {[
-                                    { href: '/#our-section', label: 'About Us' },
-                                    { href: '/#our-services', label: 'Our Services' },
-                                    { href: '/jobs', label: 'Jobs' },
-                                    { href: '/contactus', label: 'Contact Us' }
+                                    { href: '/#our-section', label: '' },
+                                    { href: '/#our-services', label: '' },
+                                    { href: '/jobs', label: '' },
+                                    { href: '/contactus', label: '' }
                                 ].map((link) => (
                                     <Link
                                         key={link.href}
